b68c32afaa251279f4961a888b2afcf4ec8ed4ea
Donovan Payan <donovan.payan@gmail.com>
2022-04-03 14:53:53 +0200

    mvp almost done

--- scripts.js
+++ scripts.js
@@ -24,3 +24,46 @@ document.querySelector("header nav").addEventListener("click", e=>{
     e.stopPropagation();
     
 })
+
+let carrouselRight = document.querySelectorAll(".icon--arrow__right");
+let carrouselLeft = document.querySelectorAll(".icon--arrow__left");
+
+
+
+carrouselRight.forEach(e => {
+    e.addEventListener("click", e=>{
+        for(let i = 1; i <= 4; i++){
+            if(window.getComputedStyle(document.querySelector(`.slide${i}`)).display === "flex"){
+                if(i === 4){
+                    document.querySelector(`.slide${i}`).style.display = "none";
+                    i = 1;
+                    document.querySelector(`.slide${i}`).style.display = "flex";
+                    break;
+                }
+                document.querySelector(`.slide${i}`).style.display = "none";
+                document.querySelector(`.slide${i+1}`).style.display = "flex";
+                break;
+            }
+        }
+    })
+})
+
+carrouselLeft.forEach(e => {
+    e.addEventListener("click", e=>{
+        for(let i = 1; i <= 4; i++){
+            if(window.getComputedStyle(document.querySelector(`.slide${i}`)).display === "flex"){
+                if(i === 1){
+                    document.querySelector(`.slide${i}`).style.display = "none";
+                    i = 4;
+                    document.querySelector(`.slide${i}`).style.display = "flex";
+                    break;
+                }
+                document.querySelector(`.slide${i}`).style.display = "none";
+                document.querySelector(`.slide${i-1}`).style.display = "flex";
+                break;
+            }
+        }
+    })
+})
+
+
